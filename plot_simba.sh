868e22f079543b0b05bdb9eda22459e4c3d4b8f9
jk-rieck <jan.klaus.rieck@gmail.com>
2026-02-09 10:13:10 -0500

    Update plot_simba.sh

--- plot_simba.sh
+++ plot_simba.sh
@@ -9,11 +9,3 @@ source /opt/anaconda3/etc/profile.d/conda.sh
 conda activate /opt/anaconda3/envs/buoy-data
 
 python3 /storage/common/buoy-data/plot-simba/plot_simba.py
-
-cp /storage/common/buoy-data/plot-simba/300340657110080_top.png /storage2/website/public_html/images/
-cp /storage/common/buoy-data/plot-simba/300340657110080_top_fr.png /storage2/website/public_html/images/
-cp /storage/common/buoy-data/plot-simba/300340657110080_bottom.png /storage2/website/public_html/images/
-cp /storage/common/buoy-data/plot-simba/300340657110080_bottom_fr.png /storage2/website/public_html/images/
-
-cp /storage/common/buoy-data/plot-simba/300340657110080_edata.json /storage2/website/public_html/data/
-cp /storage/common/buoy-data/plot-simba/300340657110080_colormap.json /storage2/website/public_html/data/
